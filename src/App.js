f2c83bc9fe92e29f49b5c51d395e398319043831
Enrique <sotomaque@gmail.com>
2020-12-24 21:01:24 -0800

    added more descriptive text to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'purple',
+      }}>
+      <ul style={{ listStyle: 'none' }}>
+        <li style={{ color: 'white' }}>
+          ✅ Set up React Redux
+        </li>
+        <li style={{ marginTop: 10, color: 'white' }}>
+          Set up React Redux
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
