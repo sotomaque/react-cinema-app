186989eb257352e59c84c61921bebcbc1a440773
Enrique <sotomaque@gmail.com>
2020-12-25 16:31:46 -0800

    slideshow component with auto increment functionality completed; temp images added in maincomponent

--- src/app/components/Content/MainContent/component.jsx
+++ src/app/components/Content/MainContent/component.jsx
@@ -5,11 +5,26 @@ import Paginated from '../../Paginated';
 import './styles.scss';
 
 const MainContent = () => {
+  const images = [
+    {
+      url: 'https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-4-5a391635d4441__700.jpg'
+    },
+    {
+      url: 'https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-29-5a3bbf6b34a98__700.jpg'
+    },
+    {
+      url: 'https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-3-5a3914a3873e1__700.jpg'
+    }
+  ];
   return (
     <div className="main-content">
       {/* Slideshow Components */}
+      <SlideShow
+        images={images}
+        auto
+        arrows
+      />
       <div className="grid-movie-title">
-        <SlideShow />
         <div className="movieType">Now Playing</div>
         <div className="paginate">
           <Paginated />
--- src/app/components/Content/SlideShow/component.jsx
+++ src/app/components/Content/SlideShow/component.jsx
@@ -1,10 +1,150 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import './styles.scss';
 
-const SlideShow = () => {
+/**
+ * Functional Component
+ *
+ * @returns Component - Image Carousel
+ *
+ * @param {array} images - array of objects with url attributes
+ * @param {boolean} auto - default = false - if true -> auto incrememnt image index every 5 seconds
+ * @param {boolean} arrows - default = false - if true -> show arrows to manually change image index
+ */
+const SlideShow = ({ images, auto = false, arrows = false }) => {
+  const [state, setState] = React.useState({
+    slideShow: images[0],
+    slideIndex: 0,
+  });
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const { slideShow, slideIndex } = state;
+
+  const [sliderInterval, setSliderInterval] = React.useState(0);
+  let currentSlideIndex = 0;
+
+  /**
+   * Hook for auto incrementing image index
+   * every 5 seconds
+   */
+  React.useEffect(() => {
+    if (auto) {
+      const timeInterval = setInterval(() => {
+        autoMoveSlide();
+      }, 5000);
+      setSliderInterval(timeInterval);
+
+      return () => {
+        clearInterval(timeInterval);
+        clearInterval(sliderInterval);
+      };
+    }
+  }, []);
+
+  /**
+   * function called to auto increment image index
+   */
+  const autoMoveSlide = () => {
+    let lastIndex = currentIndex;
+    lastIndex = currentSlideIndex + 1;
+    currentSlideIndex = lastIndex >= images.length ? 0 : lastIndex;
+    setState(prev => (
+      {
+        ...prev,
+        slideIndex: currentSlideIndex,
+        slideShow: images[currentSlideIndex]
+      }
+    ));
+  };
+
+  /**
+   * arrow click handlers
+   * @param {string} type - "prev" || "next" - default: "next"
+   */
+  const handleArrowClick = (type = 'next') => {
+    let index = currentIndex;
+    if (type === 'prev') {
+      // if going backwards from index 0
+      // go to last img
+      // else decrement index
+      currentIndex <= 0
+        ? index = images?.length - 1
+        : index -= 1;
+    } else if (type === 'next') {
+      // if going forward from index === images.lenghth - 1
+      // go to first image
+      // else incrememnt index
+      currentIndex === images?.length - 1
+        ? index = 0
+        : index += 1;
+    }
+    setCurrentIndex(index);
+    setState(prev => ({
+      ...prev,
+      slideShow: images[index],
+      slideIndex: index
+    }));
+  };
+
+  /**
+   * Functional Component
+   *
+   * renders Left and Right Arrows
+   */
+  const RenderArrows = () => {
+    return (
+      <div className="slider-arrows">
+        <div
+          className="slider-arrow slider-arrow--left"
+          onClick={() => handleArrowClick('prev')}
+        />
+        <div
+          className="slider-arrow slider-arrow--right"
+          onClick={() => handleArrowClick('next')}
+        />
+      </div>
+    );
+  };
+
+  /**
+   * Functional Component
+   *
+   * renders dots indicating index of image currently displayed
+   * @param {int} currentSlide - default 0
+   */
+  const Indicators = ({ currentSlide = 0 }) => {
+    const listIndicators = images.map((_, index) => {
+      const btnClasses =
+        index === currentSlide
+          ? 'slider-navButton slider-navButton--active'
+          : 'slider-navButton';
+      return (<button className={btnClasses} key={index} />);
+    });
+
+    return (
+      <div className="slider-nav">
+        {listIndicators}
+      </div>
+    );
+  };
+
   return (
-    <div>Slideshow</div>
+    <div className="slider">
+      <div className="slider-slides">
+        {
+          images && images.length > 0 && slideShow && (
+            <div
+              className="slider-image"
+              style={{ backgroundImage: `url(${slideShow.url})` }}
+            />
+          )
+        }
+      </div>
+      <Indicators currentSlide={slideIndex} />
+      {
+        arrows && <RenderArrows />
+      }
+    </div>
   );
 };
 
