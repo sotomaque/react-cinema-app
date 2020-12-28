c048afd4489d5d44a7c92967e9ebf93e979516b0
Enrique <sotomaque@gmail.com>
2020-12-28 06:09:57 -0800

    updated hasura urls

--- src/app/const/index.js
+++ src/app/const/index.js
@@ -5,6 +5,9 @@ export const __prod__ =
 // GQL
 export const HTTP_URL = 'http://localhost:8080/v1/graphql';
 export const WS_URL = 'ws://localhost:8080/v1/graphql';
+export const HTTPS_URL =
+  'https://cinema.hasura.app/v1/graphql';
+export const WSS_URL = 'wss://cinema.hasura.app/v1/graphql';
 
 // TMDB VARIABLES
 export const API_URL = 'https://api.themoviedb.org/3/';
