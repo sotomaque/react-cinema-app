c0a92aea61cf2fe8f35b43674a73901242ef3fb9
Enrique <sotomaque@gmail.com>
2020-12-28 15:48:59 -0800

    moved firebase envs into .env

--- src/auth.js
+++ src/auth.js
@@ -12,13 +12,16 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 // Find these options in your Firebase console
 firebase.initializeApp({
-  apiKey: 'AIzaSyAUM0akyGWC2uRer3gsjxVecrjf-Z9jPjw',
-  authDomain: 'cinema-d7621.firebaseapp.com',
-  projectId: 'cinema-d7621',
-  storageBucket: 'cinema-d7621.appspot.com',
-  messagingSenderId: '1061999019763',
-  appId: '1:1061999019763:web:f8dcb1a99930adc3819986',
-  measurementId: 'G-3B566P7LET',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const AuthContext = React.createContext();
