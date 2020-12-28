ad6181a76829c38144d0c12cced4d57c46490b61
Enrique <sotomaque@gmail.com>
2020-12-28 06:36:07 -0800

    moved link to env

--- src/app/const/index.js
+++ src/app/const/index.js
@@ -7,9 +7,8 @@ export const __prod__ =
 export const HTTP_URL = 'http://localhost:8080/v1/graphql';
 export const WS_URL = 'ws://localhost:8080/v1/graphql';
 // HASURA CLOUD + AWS LINKS
-export const HTTPS_URL =
-  'https://cinema.hasura.app/v1/graphql';
-export const WSS_URL = 'wss://cinema.hasura.app/v1/graphql';
+export const HTTPS_URL = process.env.REACT_APP_HTTPS_URL;
+export const WSS_URL = process.env.REACT_APP_WSS_URL;
 
 // TMDB VARIABLES
 export const API_URL = 'https://api.themoviedb.org/3/';
