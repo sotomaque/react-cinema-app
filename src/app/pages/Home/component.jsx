bc927d3c47af2727be8b5a745cb6513dc715ea4c
Enrique <sotomaque@gmail.com>
2020-12-28 16:22:56 -0800

    added material ui as dependancy; implmented material UI spinner in home page

--- src/app/pages/Home/component.jsx
+++ src/app/pages/Home/component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { useQuery } from '@apollo/react-hooks';
 
 import { AuthContext } from '../../../auth';
@@ -8,15 +10,36 @@ import { NUMBER_OF_USERS_QUERY } from '../../gql/queries';
 import Header from '../../components/Header';
 import Main from '../../components/Main';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '15em',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
 // TODO: ADD LOADING SPINNER COMPONENT
 const HomePage = ({ hardwareReducers }) => {
   const { authState } = React.useContext(AuthContext);
+  const classes = useStyles();
+
   console.log('authState,', authState);
   const { loading } = hardwareReducers;
   useRefreshMovies();
 
   const { data: queryData, loading: queryLoading, error } = useQuery(NUMBER_OF_USERS_QUERY);
-  if (loading || queryLoading) return (<div>Loading..</div>);
+
+  if (loading || queryLoading) {
+    return (
+      <div className={classes.root}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
   if (error) {
     console.log(error);
     return (<div>Error....</div>);
