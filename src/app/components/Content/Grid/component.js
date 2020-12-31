506317ef01f1a232c2a43b9973658a6f1929efdd
Enrique <sotomaque@gmail.com>
2020-12-30 21:46:22 -0800

    added lazyload images

--- src/app/components/Content/Grid/component.js
+++ src/app/components/Content/Grid/component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Rating from '../Rating';
 import './styles.scss';
+import LazyImage from '../../LazyImage/component';
 
 const Grid = ({ gridMovies = [] }) => {
   return (
@@ -10,44 +11,42 @@ const Grid = ({ gridMovies = [] }) => {
       {gridMovies.map((movie, index) => {
         const { url: backgroundImageUrl = '' } = movie;
         return (
-          <div key={index}>
-            <div
-              className="grid-cell"
-              style={{
-                backgroundImage: `url(${backgroundImageUrl})`,
-              }}>
-              {/* See More Button */}
-              <div className="grid-read-more">
-                <button className="grid-cell-button">
-                  See More
-                </button>
-              </div>
-              {/* Details */}
-              <div>
-                <div className="grid-detail">
-                  <span className="grid-detail-title">
-                    {movie.title}
-                  </span>
-                  <div className="grid-detail-rating">
-                    <Rating
-                      rating={
-                        movie.vote_average
-                          ? movie.vote_average
-                          : 1
-                      }
-                      totalStars={10}
-                    />
-                    &nbsp;&nbsp;
-                    <div className="grid-vote-average">
-                      {movie.vote_average
+          <LazyImage
+            key={index}
+            className="grid-cell"
+            src={`${backgroundImageUrl}`}
+            alt="placeholder">
+            {/* See More Button */}
+            <div className="grid-read-more">
+              <button className="grid-cell-button">
+                See More
+              </button>
+            </div>
+            {/* Details */}
+            <div>
+              <div className="grid-detail">
+                <span className="grid-detail-title">
+                  {movie.title}
+                </span>
+                <div className="grid-detail-rating">
+                  <Rating
+                    rating={
+                      movie.vote_average
                         ? movie.vote_average
-                        : 1}
-                    </div>
+                        : 1
+                    }
+                    totalStars={10}
+                  />
+                  &nbsp;&nbsp;
+                  <div className="grid-vote-average">
+                    {movie.vote_average
+                      ? movie.vote_average
+                      : 1}
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </LazyImage>
         );
       })}
     </div>
