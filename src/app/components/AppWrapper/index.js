a4ee992fa6d3e906b1b1655f62c2fd533c753fb6
Enrique <sotomaque@gmail.com>
2020-12-30 17:39:34 -0800

    home page clean up WIP

---
+++ src/app/components/AppWrapper/index.js
@@ -0,0 +1,3 @@
+import AppWrapper from './component';
+
+export default AppWrapper;
---
+++ src/app/components/index.js
@@ -0,0 +1,5 @@
+import AppWrapper from './AppWrapper';
+import MainContent from './Content/MainContent';
+import LoadingSpinner from './LoadingSpinner';
+
+export { AppWrapper, MainContent, LoadingSpinner };
